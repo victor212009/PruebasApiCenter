752792e825ec907db9b6da87389f40a46a2d5f5a
victor212009 <victor212009@hotmail.es>
2019-03-22 16:54:38 -0600

    Agregar archivos de proyecto.

---
+++ PruebasApiCenter/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasApiCenter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
